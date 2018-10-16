5295dccf5f82c0ee86d817180e353f0fbf09bf17
Andrew Regier <aregier@regiernet.com>
2018-10-16 13:40:49 -0500

    Save WIP for integration test

--- test/integration/OracleReader.spec.ts
+++ test/integration/OracleReader.spec.ts
@@ -1,10 +1,11 @@
-import OracleReader from "../../source/DataAccess/Oracle/OracleReader";
 import container from '../unit/test.inversify.config';
+import OracleReader from "../../source/DataAccess/Oracle/OracleReader";
 import TYPES from '../../ioc.types';
 import { Logger } from "winston";
 
 import * as chai from 'chai';
 import * as oracledb from 'oracledb';
+import { Readable } from 'stream';
 
 const expect = chai.expect;
 const should = chai.should();
@@ -15,7 +16,9 @@ describe('oracledb', () => {
         it('should return sample query results', async () => {
             var logger = container.get<Logger>(TYPES.Logger);
             let reader = new OracleReader(logger);
-            await reader.read();
+            let stream: Readable = await reader.read();
+            expect(stream.readable).to.be.equal(true);
+            // expect(stream.readableLength).to.be.greaterThan(0);
         });
     });
 });
